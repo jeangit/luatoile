da4f5d3c534f97675f0cc5b10095c134a5cea3e3
jseb <codeur2016@finiderire.com>
2020-01-16 16:35:52 +0100

    command switcher

--- luatoile.lua
+++ luatoile.lua
@@ -1,8 +1,9 @@
 #!/usr/bin/env lua
--- $$DATE$$ : mer. 15 janv. 2020 19:33:05
+-- $$DATE$$ : jeu. 16 janv. 2020 16:35:06
 
 local socket = require"socket"
 local client,server
+
 
 local function init()
   print("Running with ".. _VERSION)
@@ -15,7 +16,7 @@ local function read_header()
   local header = {}
   repeat
     line = client:receive()
-    local param, value = line:match("(%S-)%s*:%s*(.*)")
+    local param, value = line:match("^(%S-)%s*:%s*(.*)")
     if (param) then
       header[param]=value
     end
@@ -23,21 +24,38 @@ local function read_header()
   return header
 end
 
-local function mainloop()
-  client = server.accept( server)
-  local line = client:receive()
-  -- 1ère ligne requête
-  local command,path,proto = line:match("([A-Z]+)%s+(%S+)%s+(HTTP.+)")
+local function get()
   local header = read_header()
 
-  client:send(string.format('command:"%s" path:"%s" proto:"%s"\n',verb,path,proto))
   for k,v in pairs(header) do
     client:send( k .." = " .. v .. "\n")
   end
 
-  --print(client:getsockname())
+end
 
-  client:close()
+local function call_command( command, path)
+  local command_list = { GET = get }
+  if command_list[command] then
+    command_list[command]()
+  else
+    print("[error] Unknown command : client asked ",command)
+  end
+end
+
+local function mainloop()
+  while true do  
+    client = server.accept( server)
+    local line = client:receive()
+    -- 1ère ligne requête
+    local command,path,proto = line:match("([A-Z]+)%s+(%S+)%s+(HTTP.+)")
+    client:send(string.format('command:"%s" path:"%s" proto:"%s"\n',command,path,proto))
+    call_command( command, path)
+      
+
+    --print(client:getsockname())
+
+    client:close()
+  end
 end
 
 init()
