258e7e28af9548b8a6ae1d6ad26fe45f2e586cda
jseb <codeur2016@finiderire.com>
2020-01-17 16:15:47 +0100

    added urldecode, and answer for the client GET

--- luatoile.lua
+++ luatoile.lua
@@ -1,5 +1,5 @@
 #!/usr/bin/env lua
--- $$DATE$$ : ven. 17 janv. 2020 11:39:54
+-- $$DATE$$ : ven. 17 janv. 2020 16:02:53
 
 local socket = require"socket"
 local client,server
@@ -11,6 +11,17 @@ local function init()
   local port = arg[2] or 8088
   server = socket.bind("0.0.0.0", 8088)
 end
+
+local function urldecode( url)
+  local decoded = string.gsub( url, "%+", " ")
+  decoded = string.gsub( decoded, "%%(%x%x)",
+            function(char)
+                  -- receives (%x%x) and leaves longer strings
+                return string.char(tonumber(char, 16))
+            end)
+  return decoded
+end
+
 
 local function read_header()
   local header = {}
@@ -24,30 +35,43 @@ local function read_header()
   return header
 end
 
-local function whoami()
+local function whoami( header)
+  local buffer = {}
   local header = read_header()
-
   for k,v in pairs(header) do
-    client:send( k .." = " .. v .. "\n")
+    buffer[#buffer+1]= k .." = " .. v
   end
+  return table.concat(buffer,"\n")
 end
 
 local function quit()
+  --TODO check it's called locally
+  --print(client:getsockname())
   client:send("disconnected")
   is_running = false
 end
 
 
-local function get( path)
-  local special = { ["/whoami"] = whoami, ["/quit"] = quit }
+local function serve_client( buffer)
+  client:send( string.format("HTTP/1.0 200 OK\r\nserver: luatoile\r\ndate: %s\r\ncontent-type: text/html; charset=UTF-8\r\ncontent-length: %d\r\n\r\n",
+  "Lundi 35 Mai",#buffer))
+  client:send( buffer)
 
-  print(path)
+end
+
+
+local function get( path)
+  local special = { ["/whoami"] = whoami, ["/quit"] = quit , ["/"] = function() return "index.html" end }
+  local buffer = ""
+
   if special[path] then
-    special[path]()
+    buffer = special[path]()
   else
-    client:send(path)
+    local header = read_header()
+    --client:send("\n" .. urldecode(path))
     -- get file
   end
+  serve_client( buffer)
 end
 
 local function call_command( command, path)
@@ -60,16 +84,15 @@ local function call_command( command, path)
 end
 
 local function mainloop()
-  while is_running do  
+  while is_running do
     client = server.accept( server)
+    
+    -- first line : contains command, path, protocol
     local line = client:receive()
-    -- 1ère ligne requête
     local command,path,proto = line:match("([A-Z]+)%s+(%S+)%s+(HTTP.+)")
     print (string.format('command:"%s" path:"%s" proto:"%s"',command,path,proto))
-    call_command( command, path)
-      
 
-    --print(client:getsockname())
+    call_command( command, path)
 
     client:close()
   end
